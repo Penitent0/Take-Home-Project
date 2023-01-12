6e6050b53d292346031dc708bd43c23bec3f254a
Ken Lenhart <omnipaid@gmail.com>
2023-01-11 17:04:50 -0800

    Add subscriptions controller action routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post '/create', to: 'subscriptions#create'
+      get '/index', to: 'subscriptions#index'
+      patch '/update', to: 'subscriptions#update'
+    end
+  end
 end
